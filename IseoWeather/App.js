be0867a976637b585894ff3f316d26f90932fa46
유이서 <yoojh456@naver.com>
2022-10-24 17:52:11 +0900

    styles 적용

--- IseoWeather/App.js
+++ IseoWeather/App.js
@@ -1,16 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
+import { View,Text, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor:"tomato"}}>
-      {/*  자식 flex를 비율로 나타냄 */}
-      <View style={{flex:1, backgroundColor:"tomato"}}></View>
-      <View style={{flex:1,backgroundColor:"teal"}}></View>
-      <View style={{flex:1,backgroundColor:"orange"}}></View>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor:"tomato"
+  },
+  city: {
+    flex: 1,
+    backgroundColor: "yellow",
+    justifyContent: "center",
+    alignItems:"center",
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight:"500",
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    flex: 1,
+    // justifyContent: "center",
+    alignItems:"center",
+    backgroundColor:"blue",
+  },
+  temp: {
+    marginTop:50,
+    fontSize:158,
+  },
+  description: {
+    fontSize: 60,
+    marginTop:-30,
+  }
+})
 
 // const styles = StyleSheet.create({
 //   container: {
