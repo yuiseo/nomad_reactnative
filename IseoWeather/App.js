d785bf08a2414d673a5e8dd96fad9f8afe2c046f
유이서 <yoojh456@naver.com>
2022-10-24 17:32:24 +0900

    flex 적용

--- IseoWeather/App.js
+++ IseoWeather/App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>iseo!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor:"tomato"}}>
+      {/*  자식 flex를 비율로 나타냄 */}
+      <View style={{flex:1, backgroundColor:"tomato"}}></View>
+      <View style={{flex:1,backgroundColor:"teal"}}></View>
+      <View style={{flex:1,backgroundColor:"orange"}}></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   text: {
+//     fontSize:29,
+//   }
+// });
