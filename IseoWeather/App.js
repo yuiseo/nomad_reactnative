419c258a3ad98a0fb35a4c3722d71e1a99446da6
유이서 <yoojh456@naver.com>
2022-10-25 16:16:17 +0900

    weather

--- IseoWeather/App.js
+++ IseoWeather/App.js
@@ -1,15 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React,{ useEffect,useState } from 'react';
-import { View,Text, Dimensions, StyleSheet, ScrollView } from 'react-native';
+import { View,Text, Dimensions, StyleSheet, ScrollView, ActivityIndicator } from 'react-native';
 import * as Location from 'expo-location';
 const { width:SCREEN_WIDTH } = Dimensions.get('window');
-
+const API_KEY = "f9ecf35e904a8f11952563a586d514c2"
 console.log(SCREEN_WIDTH)
 export default function App() {
-  const [city,setCity] = useState("Loading...")
-  const [location, setLocation] = useState();
+  const [city, setCity] = useState("Loading...");
+  const [days, setDays] = useState([]);
   const [ok, setOk] = useState(true);
-  const ask = async () => {
+  const getWeather = async () => {
     // const permission = await Location.requestForegroundPermissionsAsync();
     const {granted} = await Location.requestForegroundPermissionsAsync();
     if (!granted) {
@@ -19,9 +19,15 @@ export default function App() {
     const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
     const location = await Location.reverseGeocodeAsync({latitude,longitude},{useGoogleMaps:false})
     setCity(location[0].city)
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`
+    );
+    const json = await response.json();
+    setDays(json.daily);
+
   }
   useEffect(() => {
-    ask()
+    getWeather()
   }, [])
   
   return (
@@ -35,22 +41,25 @@ export default function App() {
         //indicatorStyle='white'
         showsHorizontalScrollIndicator={false}
         contentContainerstyle={styles.weather}>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>sunny</Text>
-        </View>
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator
+              color="white"
+              style={{ marginTop: 10 }}
+              size="large"
+            />
+          </View>
+        ) : (
+          days.map((day, index) => (
+            <View key={index} style={styles.day}>
+              <Text style={styles.temp}>
+                {parseFloat(day.temp.day).toFixed(1)}
+              </Text>
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+            </View>
+          ))
+        )}
       </ScrollView>
     </View>
   );
